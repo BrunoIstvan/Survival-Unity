7bd30d027ad71df58a0f86993adac64d1a5910d3
fnsilva23 <fn.silva23@gmail.com>
2019-03-31 12:09:02 -0300

    Criação do PowerUp guarda chuva

---
+++ Assets/Scripts/GerarPowerUpScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GerarPowerUpScript : MonoBehaviour
+{
+
+    public float tempoInicial, tempoFinal;
+    public float esquerdaX, direitaX;
+    public float topoY, embaixoY;
+    public GameObject[] powerUpPrefab;
+    public float tempoItem;
+
+    void Start()
+    {
+        StartCoroutine(Gerar());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+     IEnumerator Gerar()
+     {
+
+         int index = Random.Range(0, powerUpPrefab.Length);
+
+         // aguarda um tempo para gerar PowerUP
+         yield return new WaitForSeconds(tempoItem);
+
+         // gera uma posição em X... usa a posição y e z do ponto que está na tela...
+         Vector3 posicao = new Vector3(Random.Range(esquerdaX, direitaX),
+                                         Random.Range(embaixoY, topoY), transform.position.z);
+         // instancia o inimigo
+         Instantiate(powerUpPrefab[index], posicao, transform.rotation);
+
+         // executar uma Coroutine
+         StartCoroutine(Gerar());
+
+     }
+}
---
+++ Assets/Scripts/ItemGuardaChuva.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGuardaChuva : MonoBehaviour
+{
+
+    //Power UP Guarda Chuva
+    public GameObject PUGuardaChuva;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.tag == "Escudo")
+        {
+            print("bateu no escudo");
+            //Instantiate(PUGuardaChuva, collision.gameObject.transform.position, transform.rotation);
+            //Instantiate(PUGuardaChuva, transform);
+            //collision.gameObject.transform.parent=transform;
+            print(collision.gameObject.transform.GetChild(0));
+            GameObject escudo = collision.gameObject.transform.GetChild(0).gameObject;
+
+            escudo.SetActive(true);
+            Destroy(gameObject, 0.0f);
+        }
+
+        if (collision.gameObject.tag == "Inimigo")
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), this.GetComponent<Collider2D>(), true);
+        }
+
+        if (collision.gameObject.tag == "Finish")
+        {
+            Destroy(gameObject, 0.0f);
+        }
+    }
+}
